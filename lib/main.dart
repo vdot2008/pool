24e544d83098805fad679fe5e3538fce0aa8be4e
vdot2008 <40758607+vdot2008@users.noreply.github.com>
2022-09-08 17:03:54 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:typed_data';
 import 'package:pool/widgets/splash_screen.dart';
 import 'package:pool/widgets/webview_screen.dart';
 import 'package:flutter/material.dart';
@@ -30,43 +29,4 @@ class _MyAppState extends State<MyApp> {
           'webview': (BuildContext context) => const WebviewScreen(),
         });
   }
-}
-
-late final String title;
-Widget build(BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: PdfPreview(
-        build: (format) => _generatePdf(format, title),
-      ),
-    ),
-  );
-}
-
-Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
-  final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
-  final font = await PdfGoogleFonts.nunitoExtraLight();
-
-  pdf.addPage(
-    pw.Page(
-      pageFormat: format,
-      build: (context) {
-        return pw.Column(
-          children: [
-            pw.SizedBox(
-              width: double.infinity,
-              child: pw.FittedBox(
-                child: pw.Text(title, style: pw.TextStyle(font: font)),
-              ),
-            ),
-            pw.SizedBox(height: 20),
-            pw.Flexible(child: pw.FlutterLogo())
-          ],
-        );
-      },
-    ),
-  );
-
-  return pdf.save();
 }
